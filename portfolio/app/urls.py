01b5d77c9bcf367d1285df927ee3ba8d344ef950
das611821@gmail.com <das611821@gmail.com>
2025-06-20 11:30:37 +0530

    initial commit

--- portfolio/app/urls.py
+++ portfolio/app/urls.py
@@ -3,7 +3,4 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('about/', views.about, name='about'),
-    # path('experience/', views.experience, name='experience'),
-    # path('contact_form/', views.contact_form, name='contact_form'),
 ]
